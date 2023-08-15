908bc412e70d40a43774d4ada177d289e2f11581
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-15 15:51:29 -0400

    Menus componenets created
    styles added

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Home from '../../pages/Home/Home'
-import { AiOutlineDoubleLeft } from 'react-icons/ai';
+import { AiOutlineDoubleLeft ,AiOutlineDoubleRight} from 'react-icons/ai';
+import './Layout.css';
+import Menus from '../Menus/Menus';
  
 const Layout = () => {
+    const [toggle,setToggle] =useState(true)
+
+
+    //change toggle function
+    const handleToggle = () =>{
+        setToggle(!toggle);
+    }
   return (
     <>
     
-    <div className=".sidebar-section"></div>
-    <div className='sidebar'></div>
-        <div className='sidebar-toggle-icons'>
-           <p>
-            <AiOutlineDoubleLeft size={30} />
-           </p>
+    <div className="sidebar-section">
+        <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
+          <div className="sidebar-toggle-icons">
+            <p onClick={handleToggle}>
+              {toggle ? (
+                <AiOutlineDoubleLeft size={30} />
+              ) : (
+                <AiOutlineDoubleRight size={30} />
+              )}
+            </p>
+          </div>
+          <Menus toggle={toggle} />
         </div>
-        <div className='container'></div>
-        <Home />
+        <div className="container">
+          <Home />
+        </div>
+      </div>
     </>
   )
 }
 
-export default Layout
+export default Layout;
---
+++ client/src/components/Menus/Menus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Menus.css";
+
+const Menus = () => {
+  return (
+    <>
+    <div className='navnar-profile-pic'>
+
+        <img src="" alt="profile pic" />
+    </div>
+    
+    </>
+  )
+}
+
+export default Menus
