870200a29307abb491e88482363628c9f8a9b7c8
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-15 17:43:52 -0400

    menue icon collapse icons created and title added
    title will be displayed as hovered on icons

--- client/src/components/Menus/Menus.js
+++ client/src/components/Menus/Menus.js
@@ -6,7 +6,9 @@ import { FcBriefcase } from 'react-icons/fc';
 import { FcAutomatic } from 'react-icons/fc';
 import { FcGraduationCap } from 'react-icons/fc'
 import { FcBullish } from 'react-icons/fc'
-import { FcBusinessContact } from 'react-icons/fc'
+import { FcBusinessContact } from 'react-icons/fc';
+import{FcVoicePresentation} from 'react-icons/fc';
+
 const Menus = ({toggle}) => {
   return (
     <>
@@ -41,6 +43,9 @@ const Menus = ({toggle}) => {
                     <FcBullish /> Projects
                     </div>
                     <div className="nav-link">
+                    <FcVoicePresentation /> Testomonials
+                    </div>
+                    <div className="nav-link">
                     <FcBusinessContact /> Contact
                     </div>
                     
@@ -50,8 +55,41 @@ const Menus = ({toggle}) => {
             </div>
         </>
         ) : (
+            <>
+                <div className="nav-items">
+                <div className="nav-item">
+                    <div className="nav-link">
+                    <FcHome title="Home"/> 
+                    </div>
+                    <div className="nav-link">
+                    <FcAbout title="About"/>
+                    </div>
+                    <div className="nav-link">
+                    <FcBriefcase title="Work Experience"/> 
+                    </div>
+                    <div className="nav-link">
+                    <FcAutomatic title="Tech Stack"/> 
+                    </div>
+                    <div className="nav-link">
+                    <FcGraduationCap title="Education"/>  
+                    </div>
+                    <div className="nav-link">
+                    <FcBullish title="Projects"/> 
+                    </div>
+                    <div className="nav-link">
+                    <FcVoicePresentation title="Testomonials"/> 
+                    </div>
+                    <div className="nav-link">
+                    <FcBusinessContact title="Contact"/> 
+                    </div>
+                    
+                    
+                    
+                </div>
+            </div>
+
+            </>
         
-         <h1>Icons</h1>
          )}
 
     </>
