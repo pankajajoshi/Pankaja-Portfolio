13c5dc5e8e8c757daf15a5ffa2f74fe04b8872cf
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-15 16:31:59 -0400

    Home tab added
    profile picture added
    
    styles added for Home and Profilepic

--- client/src/components/Menus/Menus.js
+++ client/src/components/Menus/Menus.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import "./Menus.css";
+import { FcHome } from 'react-icons/fc';
 
-const Menus = () => {
+const Menus = ({toggle}) => {
   return (
     <>
-    <div className='navnar-profile-pic'>
+        {toggle ? (
+        <>
+         
+        
+         <div className="navbar-profile-pic">
 
-        <img src="" alt="profile pic" />
-    </div>
-    
+            <img 
+            src="https://allthings.how/content/images/wordpress/2020/10/allthings.how-how-to-change-your-profile-picture-on-google-meet-profile-photo.png" alt="profile pic" 
+            />
+        </div>
+            <div className="nav-items">
+                <div className="nav-item">
+                    <div className="nav-link">
+                    <FcHome /> Home
+                    </div>
+                    
+                </div>
+            </div>
+        </>
+        ) : (
+        
+         <h1>Icons</h1>
+         )}
+
     </>
-  )
-}
+  );
+};
 
-export default Menus
+export default Menus;
