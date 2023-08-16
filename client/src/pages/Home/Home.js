4a4e05c8f5ce6b6d2cb9c79dc8bd146b4bde48a8
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-16 14:50:41 -0400

    buttons for resume and hire me added
    styles and trasitions added to buttons

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import './home.css';
+import React from "react";
+import "./home.css";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
     <>
       <div className="container-fluid home-container">
         <div className="container home-content">
-
           <h1>Hi i am PK</h1>
-
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "FullStack Developer !",
+                  "Software Engineer !",
+                  "React Developer !",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me</button>
+            <button className="btn btn-cv">PK-Resume</button>
+          </div>
         </div>
-        
-      </div> 
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
